64f6b9a173d97f8072777cb2b83c3425943b0074
VaishnaviSahu <vaishnavi.sahu@cgi.com>
2018-09-12 15:42:35 +0530

    .net4

--- Transform/Transform.cs
+++ Transform/Transform.cs
@@ -4,11 +4,18 @@ using System.Collections.Generic;
 
 namespace Transform
 {
+     public delegate int ExpressionDelegate(int number);
     public static class Transform 
     {
-        public static void Map()
+          public static int[] Map(this int[] numbers, ExpressionDelegate expressionDelegate)
         {
-            throw new NotImplementedException();
+            int[] result = new int[numbers.Length];
+            for(int i = 0; i < result.Length; i++)
+            {
+                result[i] = expressionDelegate(numbers[i]);
+            }
+            return result;
         }
+        
     }
 }
